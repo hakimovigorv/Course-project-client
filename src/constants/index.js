b74c54838f6bb6da0f75f06040f09a9871843ab8
hakimovigorv <hakimovigorv@gmail.com>
2022-03-27 01:52:09 +0300

    change API URL to debloy

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = `http://localhost:8080/`;
+export const API_BASE_URL = `https://course-project-hakimovigorv.herokuapp.com/`;
 export const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/hakimovigorv/upload`;
